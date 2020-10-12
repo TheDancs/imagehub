d66a26f3f74c0627af7c3015c5ad0aa821cb1131
Kusnyár Ádám <kusnyaradam@office42.hu>
2020-10-13 01:40:55 +0200

    Upadate

--- src/components/alert.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Alert() {
-    return (
-        <Alert variant="filled" severity="error">
-            Sikertelen bejelentkezés!
-        </Alert>
-    )
-}
--- src/components/feed.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Feed() {
-    return (
-        <div>
-            <p className="main--content">Feed</p>
-        </div>
-    )
-}
