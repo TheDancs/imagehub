fd035b23fded84b33ee3618a784978fea26f7f27
Dániel Falus <dfalus@geomant.com>
2020-10-13 22:26:19 +0200

    Fix upload bug

--- image-hub-frontend/src/components/upload.js
+++ image-hub-frontend/src/components/upload.js
@@ -8,47 +8,51 @@ export function Upload() {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
-
-  const submitForm = () => {};
-
+  
   const handleSubmit = function (e) {
     const form = new FormData();
     form.append("Title", title);
     form.append("Description", description);
-    form.append("UploaderId ", userData.userID);
+    form.append("UploaderId", userData.userID);
     form.append("File", selectedFile);
 
     const response = axios({
       method: "post",
-      url: "https://localhost:5001/api/v1.0/Image",
+      url: "https://imagehub.azurewebsites.net/api/v1.0/Image",
       data: form,
-      headers: {
-        "content-type": 'multipart/form-data; boundary=${form._boundary}',
-      },
-    });
+    })
+      .then((data) => {
+        alert("success");
+      })
+      .catch((error) => {
+        alert("errot");
+      });
+
     e.preventDefault();
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h1>File Upload</h1>
-        <input
-          type="file"
-          onChange={(e) => setSelectedFile(e.target.files[0])}
-        />
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <input
-          type="text"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-        <button type="submit">Upload</button>
-      </form>
+    <div className="container">
+      <div className="image-placeholder">
+        <form onSubmit={handleSubmit}>
+          <h1>File Upload</h1>
+          <input
+            type="file"
+            onChange={(e) => setSelectedFile(e.target.files[0])}
+          />
+          <input
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <input
+            type="text"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          <button type="submit">Upload</button>
+        </form>
+      </div>
     </div>
   );
 }
