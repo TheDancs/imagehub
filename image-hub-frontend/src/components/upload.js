5f4599446acd2bf5adedeead246cde334f4cb44c
Dániel Falus <dfalus@geomant.com>
2020-10-13 22:50:02 +0200

    Add snackbar

--- image-hub-frontend/src/components/upload.js
+++ image-hub-frontend/src/components/upload.js
@@ -1,14 +1,35 @@
 import React, { useContext, useState } from "react";
 import UserDataContext from "../context/UserDataContext";
 import axios from "axios";
+import Button from "@material-ui/core/Button";
+import Snackbar from "@material-ui/core/Snackbar";
+import MuiAlert from "@material-ui/lab/Alert";
+import { makeStyles } from "@material-ui/core/styles";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    "& > * + *": {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
 
 export function Upload() {
-  const userData = useContext(UserDataContext);
+  const classes = useStyles();
 
+  const [open, setOpen] = useState(false);
+  const [result, setResult] = useState(false);
+
+  const userData = useContext(UserDataContext);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [selectedFile, setSelectedFile] = useState(null);
-  
+
   const handleSubmit = function (e) {
     const form = new FormData();
     form.append("Title", title);
@@ -18,21 +39,30 @@ export function Upload() {
 
     const response = axios({
       method: "post",
-      url: "https://imagehub.azurewebsites.net/api/v1.0/Image",
+      url: "https://imagehub.azurewebsites.net/api/v1.0/Iage",
       data: form,
     })
       .then((data) => {
-        alert("success");
+        setOpen(true);
+        setResult(true);
       })
       .catch((error) => {
-        alert("errot");
+        setOpen(true);
       });
 
     e.preventDefault();
   };
 
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
-    <div className="container">
+    <div className={classes.root}>
       <div className="image-placeholder">
         <form onSubmit={handleSubmit}>
           <h1>File Upload</h1>
@@ -53,6 +83,11 @@ export function Upload() {
           <button type="submit">Upload</button>
         </form>
       </div>
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity={result ? "success" : "error"}>
+          {result ? "Successfully uploaded 🎉" : "Upload failed 😭"}
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
