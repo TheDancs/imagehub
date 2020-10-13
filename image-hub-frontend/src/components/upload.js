1de1c6752766e3e9051debc83ac9ac4fc8ff695f
Dániel Falus <dfalus@geomant.com>
2020-10-13 23:01:56 +0200

    Add loading screen

--- image-hub-frontend/src/components/upload.js
+++ image-hub-frontend/src/components/upload.js
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from "react";
 import UserDataContext from "../context/UserDataContext";
 import axios from "axios";
-import Button from "@material-ui/core/Button";
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
 import { makeStyles } from "@material-ui/core/styles";
@@ -17,10 +18,16 @@ const useStyles = makeStyles((theme) => ({
       marginTop: theme.spacing(2),
     },
   },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
 }));
 
 export function Upload() {
   const classes = useStyles();
+
+  const [loading, setLoading] = useState(false);
 
   const [open, setOpen] = useState(false);
   const [result, setResult] = useState(false);
@@ -31,6 +38,9 @@ export function Upload() {
   const [selectedFile, setSelectedFile] = useState(null);
 
   const handleSubmit = function (e) {
+
+    setLoading(true);
+
     const form = new FormData();
     form.append("Title", title);
     form.append("Description", description);
@@ -39,15 +49,17 @@ export function Upload() {
 
     const response = axios({
       method: "post",
-      url: "https://imagehub.azurewebsites.net/api/v1.0/Iage",
+      url: "https://imagehub.azurewebsites.net/api/v1.0/Image",
       data: form,
     })
       .then((data) => {
         setOpen(true);
         setResult(true);
+        setLoading(false);
       })
       .catch((error) => {
         setOpen(true);
+        setLoading(false);
       });
 
     e.preventDefault();
@@ -59,6 +71,14 @@ export function Upload() {
     }
 
     setOpen(false);
+  };
+
+  const handleCloseLoading = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setLoading(false);
   };
 
   return (
@@ -82,7 +102,17 @@ export function Upload() {
           />
           <button type="submit">Upload</button>
         </form>
+
+        <Backdrop
+          className={classes.backdrop}
+          open={loading}
+          onClick={handleCloseLoading}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+
       </div>
+
       <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
         <Alert onClose={handleClose} severity={result ? "success" : "error"}>
           {result ? "Successfully uploaded 🎉" : "Upload failed 😭"}
