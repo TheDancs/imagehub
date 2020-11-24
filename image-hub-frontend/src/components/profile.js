a653d7e8e536fdb61d108433877a4a5b38045bb0
Károly Kovács <donciking@gmail.com>
2020-11-24 20:07:31 +0100

    profile page improved (not finished)

--- image-hub-frontend/src/components/profile.js
+++ image-hub-frontend/src/components/profile.js
@@ -1,277 +1,227 @@
-import React, { useState, useEffect,useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import UserDataContext from "../context/UserDataContext";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardHeader from '@material-ui/core/CardHeader';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import CardActions from '@material-ui/core/CardActions';
-import Avatar from '@material-ui/core/Avatar';
-import IconButton from '@material-ui/core/IconButton';
 import { red } from '@material-ui/core/colors';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import Modal from '@material-ui/core/Modal';
 import Button from '@material-ui/core/Button';
+import RecipeReviewCard from "./post";
+import Paper from '@material-ui/core/Paper';
+import GridList from '@material-ui/core/GridList';
+import TransitionsModal, { FrienRequests, ViewFriends } from "./modals";
+import Avatar from '@material-ui/core/Avatar';
+import Grid from '@material-ui/core/Grid';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import { ShowError } from "./alert";
+
 
 const useStyles = makeStyles((theme) => ({
-    container:{
-        width: 450,
-        marginRight: "auto",
-        marginLeft: "auto",
+  container: {
+    width: 450,
+    marginRight: "auto",
+    marginLeft: "auto",
+  },
+  root: {
+    maxWidth: 450,
+    marginBottom: 15,
+  },
+  media: {
+    height: 0,
+    paddingTop: '100.00%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatarLarge: {
+    width: theme.spacing(7),
+    height: theme.spacing(7),
+  },
+  avatarProfile: {
+    width: theme.spacing(20),
+    height: theme.spacing(20),
+  },
+  paper: {
+    position: 'absolute',
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  paperRoot: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    '& > *': {
+      margin: theme.spacing(1),
+      width: theme.spacing(16),
+      height: theme.spacing(16),
     },
-    root: {
-        maxWidth: 450,
-        marginBottom: 15,
-    },
-    media: {
-        height: 0,
-        paddingTop: '100.00%', // 16:9
-    },
-    expand: {
-        transform: 'rotate(0deg)',
-        marginLeft: 'auto',
-        transition: theme.transitions.create('transform', {
-            duration: theme.transitions.duration.shortest,
-        }),
-    },
-    expandOpen: {
-        transform: 'rotate(180deg)',
-    },
-    avatar: {
-        backgroundColor: red[500],
-    },
-    paper: {
-        position: 'absolute',
-        width: 400,
-        backgroundColor: theme.palette.background.paper,
-        border: '2px solid #000',
-        boxShadow: theme.shadows[5],
-        padding: theme.spacing(2, 4, 3),
-      },
-      
+  },
 }));
 
-function rand() {
-  return Math.round(Math.random() * 20) - 10;
-}
 
-function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+export function Profile(id = 'me') {
+  if(typeof id != typeof string)
+    id ="me";
 
-  return {
-    top: `${top}%`,
-    left: `${left}%`,
-    transform: `translate(-${top}%, -${left}%)`,
-  };
-}
-
-export function ViewFriends() {
-  const classes = useStyles();
-  // getModalStyle is not a pure function, we roll the style only on the first render
-  const [modalStyle] = React.useState(getModalStyle);
-  const [open, setOpen] = React.useState(false);
-  const [friends, setFriends] = useState([]);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  
-  useEffect(() => {
-    fetch("https://imagehub.azurewebsites.net/api/v2.0/User/me")
-  .then(res => res.json())
-  .then(
-    (result) => {
-      setFriends(result.friends);
-    }
-  )
-}, [])
-
-  const body = (
-    <div style={modalStyle} className={classes.paper}>
-      <h2 id="simple-modal-title">Friends</h2>
-      <p id="simple-modal-description">
-        {friends.map((friend)=> {
-                return (
-                <p>{friend.name}</p>
-                );
-        })
-        }
-      </p>
-    </div>
-  );
-
-  return (
-    <div>
-      <Button variant="outlined" onClick={handleOpen}>
-        Friend list
-      </Button>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="simple-modal-title"
-        aria-describedby="simple-modal-description"
-      >
-        {body}
-      </Modal>
-    </div>
-  );
-}
-export function FrienRequests() {
-    const classes = useStyles();
-    // getModalStyle is not a pure function, we roll the style only on the first render
-    const [modalStyle] = React.useState(getModalStyle);
-    const [open, setOpen] = React.useState(false);
-    const [requests, setRequests] = useState([]);
-  
-    const handleOpen = () => {
-      setOpen(true);
-    };
-  
-    const handleClose = () => {
-      setOpen(false);
-    };
-
-    const acceptRequest = (id) => {
-            fetch("https://imagehub.azurewebsites.net/api/v2.0/User/friendrequests/"+{id}+"/accept");
-
-    };
-    const rejectRequest = (id) => {
-        
-            fetch("https://imagehub.azurewebsites.net/api/v2.0/User/friendrequests/"+{id}+"/reject");
-    };
-  
-    
-    useEffect(() => {
-      fetch("https://imagehub.azurewebsites.net/api/v2.0/User/friendrequests")
-    .then(res => res.json())
-    .then(
-      (result) => {
-        setRequests(result);
-      }
-    )
-  }, [])
-  
-    const body = (
-      <div style={modalStyle} className={classes.paper}>
-        <h2 id="simple-modal-title">Friend requests</h2>
-        <p id="simple-modal-description">
-          {requests.map((req)=> {
-                  return (
-                      <div>
-                    <p>{req.from.name}</p>
-                  <Button variant="contained" onClick={acceptRequest(req.id)}>Accept</Button>
-                  <Button variant="outlined" onClick={rejectRequest(req.id)}>Reject</Button>
-                      </div>                  
-                  );
-          })
-          }
-        </p>
-      </div>
-    );
-  
+  var profileUrl = "https://imagehub.azurewebsites.net/api/v2.0/User/"+id;
+  var user;
+  user = useContext(UserDataContext);
+  if(id != "me" && user == null){
+    user = FetchUrl(profileUrl).
+    catch(error =>{
+      console.log(error);
+    } ) 
+  }
+ 
+  var frnd = [{name:"csicska 3idjf"}]
+  var posts = UserPosts(id);
+  var mockuser = {id:22, name: "Károly Kovács", friends: frnd, posts: posts };
+    //Fetch user
     return (
-      <div>
-        <Button variant="outlined" onClick={handleOpen}>
-          Friend Requests
-        </Button>
-        <Modal
-          open={open}
-          onClose={handleClose}
-          aria-labelledby="simple-modal-title"
-          aria-describedby="simple-modal-description"
-        >
-          {body}
-        </Modal>
-      </div>
-    );
-}
-
-export function Profile() {
-    
-    const userData = useContext(UserDataContext);
-    var url = "https://imagehub.azurewebsites.net/api/v2.0/Feed/user/"+userData.userId;
-
-    const [Posts, setPosts] = useState([]);
-    const [error, setError] = useState(null);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [friends, setFriends] = useState([]);
-    useEffect(() => {
-        fetch(url)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setIsLoaded(true);
-          setPosts(result);
-        },
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [])
-  useEffect(() => {
-    fetch("https://imagehub.azurewebsites.net/api/v2.0/User/me")
-  .then(res => res.json())
-  .then(
-    (result) => {
-      setFriends(result.friends);
-    }
-  )
-}, [])
-  const classes = useStyles();
-    return (
-        <Container maxWidth="lg">
-            <Typography className="main--content">
-                <div className="user--data">
-                    <h1>{userData.name}</h1>
-                    <p>{Posts.length} bejegyzés, {friends.length} barát</p>
-                    <p><ViewFriends/> </p>
-                    <p><FrienRequests/></p>
-                </div>
-                
-            <p> {Posts.map((post) => {
-                return (
-                    <Card className={classes.root}>
-                        <CardHeader
-                            avatar={
-                                <Avatar aria-label="recipe" className={classes.avatar}>
-                                    {(post.uploader.name ?post.uploader.name.charAt(0) : "A")}
-                                </Avatar>
-                            }
-                            title={post.uploader.name}
-                        />
-                        <CardMedia
-                            className={classes.media}
-                            image={post.pictureUrl}
-                        />
-                        <CardContent>
-                            <Typography variant="body2" color="textSecondary" component="p">
-                                {post.description}
-                            </Typography>
-                        </CardContent>
-                        <CardActions disableSpacing>
-                            <IconButton aria-label="add to favorites">
-                                <FavoriteIcon />
-                                {post.likes}
-                            </IconButton>
-
-                        </CardActions>
-                    </Card>
-                );
+      <Container>
+        <Typography>
+          {ProfileSummary(user)}
+          <GridList cols="3" width="800">
+            {posts.map((post) => {
+              return (
+                RecipeReviewCard(post)
+              );
             }
-
+  
             )
-            }</p>
-            </Typography>
-        </Container>
-
+            }
+          </GridList>
+  
+        </Typography>
+     </Container>
+  
     )
+  
+
+
+}
+
+
+export function ProfileSummary(user) {
+  const classes = useStyles();
+  var requests;
+  var me = useContext(UserDataContext);
+    
+    var rqButton = (<Button >Send FriendRequest</Button>);
+    if(me === user){
+      console.log("!user");
+      
+      console.log(user.name);
+      requests = GetFriendRequests().catch(error => { console.log(error)});
+      rqButton = (FrienRequests(requests))
+    }
+    
+   
+    return (
+      <div className="main--content">
+        <Paper elevation={1} className="paper--profile" variant="elevation">
+          <Grid container spacing={5}>
+            <Grid item justify="center" alignContent="center">
+              <Avatar src={user.profilPicture} className={classes.avatarProfile} />
+            </Grid>
+            <Grid item xs={12} sm container>
+              <Grid item container direction="column" alignItems="stretch" justify="center">
+                <Grid item xs>
+                  <Typography variant="h4">
+                    {user.name}
+                  </Typography>
+               </Grid>
+                    <Grid item xs>
+                      <Typography variant="subtitle1" gutterBottom>
+                        {user.posts?.length} Posts  |  {user.friends?.length} Friends
+                  </Typography>
+                  </Grid>
+
+
+                <Grid item xs>
+                  <Grid container spacing={2}
+                    direction="row">
+                    <Grid item >
+                      {ViewFriends(user.friends)}
+
+                    </Grid>
+                    <Grid item>
+                      {rqButton}
+                    </Grid>
+
+
+                  </Grid>
+
+                </Grid>
+
+              </Grid>
+            </Grid>
+          </Grid>
+        </Paper>
+      </div>
+    );
+  
+  
+
+  }
+
+
+
+
+
+
+
+export function UserPosts(userid) {
+  var feedUrl = "https://imagehub.azurewebsites.net/api/v2.0/Feed/user/" + userid;
+  //var feed = FetchUrl(feedUrl);
+  var feed = [{ title: "Post1", description: "Desc1", pictureUrl: "asdfad", likes: 10, uploader: { name: "adfads", id: "asdfa" } },
+  { title: "Post1s", description: "Desc1s", pictureUrl: "asdfad", likes: 10, uploader: { name: "adfads", id: "asdfa" } }]
+  return feed;
+}
+
+async function GetFriendRequests() {
+  console.log("Function: GetFriendRequests");
+  var url = "https://imagehub.azurewebsites.net/api/v2.0/User/friendrequests"
+  var requests;
+  try{
+    requests = await FetchUrl(url);
+    return requests;
+  }
+  catch(err){
+    throw new Error("Couldn't load Friend requests because: \n" + err.message)
+  }
+  
+
+  
+}
+
+export async function FetchUrl(url){
+  const valasz = await fetch(url);
+  
+  if(!valasz.ok){
+    if(valasz.status == 401)
+      throw new Error('You are not logged in.');
+    throw new Error('Unknown error (Something wrong with the network response).');
+  }
+  else{
+    var ansf = valasz.json();
+    return ansf;
+  }
+  
+}
+
+async function postData(url = '') {
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
 }
